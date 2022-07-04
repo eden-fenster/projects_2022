ed16732187f696c59cbb75b574d0420053a6b472
eden <eden.fenster@gmail.com>
2022-07-04 06:33:31 +0300

    The Bul Pgia game using modules.

---
+++ bul_pgia_modules/generate_number.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import random
+
+
+def generate_number(number_of_digits: int, can_repeat_digits: bool) -> str:
+    i: int = 0
+    number: str = ''
+    if can_repeat_digits:
+        while i < number_of_digits:
+            if i == 0:
+                digit = random.randint(1, 9)
+                number += str(digit)
+            else:
+                digit = random.randint(0, 9)
+                number += str(digit)
+            i += 1
+    else:
+        while i < number_of_digits:
+            if i == 0:
+                digit = random.randint(1, 9)
+                number += str(digit)
+            else:
+                digit = random.randint(0, 9)
+                if str(digit) in number:
+                    continue
+                else:
+                    number += str(digit)
+
+            i += 1
+    return number
