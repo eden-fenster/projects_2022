f6cf552ff54c2df653e21531d48eeed3edc50e54
eden <eden.fenster@gmail.com>
2022-07-28 21:12:10 +0300

    Made code more readable.

--- random_snack_generator/random_snack_generator_app.py
+++ random_snack_generator/random_snack_generator_app.py
@@ -58,15 +58,20 @@ def main() -> None:
 
     if add_snack:
         num: str = input('How many snack do you want to add ?') + '\n'
+
         while not num.isdigit() or int(num) < 1:
             num: str = input('Not a vaild number, try again !') + '\n'
+
         num_to_add: int = int(num)
+
         if num_to_add == 1:
             snack_name: str = input('Input snack name') + '\n'
             snack_availability: str = input('Type True for yes and False for no') + '\n'
             food_list.add_one(snack_name, snack_availability)
+
         if num_to_add > 1:
             snacks_to_add: List[Tuple[str, str]] = []
+
             for i in range(0, num_to_add):
                 snack_name: str = input('Input snack name') + '\n'
                 snack_availability: str = input('Type True for yes and False for no') + '\n'
@@ -77,8 +82,10 @@ def main() -> None:
     if delete_snack:
         food_to_delete: str = input('What food do you want to delete ?') + '\n'
         one_or_all: str = input('Do you want to delete one or all instances ? type One for one and All for all') + '\n'
+
         if one_or_all == 'One':
             food_list.delete_one(food_to_delete)
+
         if one_or_all == 'All':
             food_list.delete_many(food_to_delete)
 
@@ -86,9 +93,11 @@ def main() -> None:
         name_or_availability: str = \
             input('Do you want to look up by name or by availability ? '
                   'type Name for name and Availability for availability') + '\n'
+
         if name_or_availability == 'Name':
             name: str = input('Type name of snack') + '\n'
             food_list.food_lookup_by_name(name)
+
         if name_or_availability == 'Availability':
             condition: str = input('Type True for available and False for not available') + '\n'
             food_list.food_lookup_by_availability(condition)
@@ -96,22 +105,29 @@ def main() -> None:
     if print_snacks:
         random_or_all: str = \
             input('Do you want a random snack or all snacks printed ? type Random for random and All for all') + '\n'
+
         if random_or_all == 'Random':
             food_list.choose_random()
+
         if random_or_all == 'All':
             food_list.show_all()
 
     if change_availability:
         num: str = input('How many snack do you want to change ?') + '\n'
+
         while not num.isdigit() or int(num) < 1:
             num: str = input('Not a vaild number, try again !') + '\n'
+
         num_to_change: int = int(num)
+
         if num_to_change == 1:
             snack_name: str = input('Input snack name') + '\n'
             snack_availability: str = input('Type True for yes and False for no') + '\n'
             food_list.change_availability(snack_name, snack_availability)
+
         if num_to_change > 1:
             i: int = 0
+
             while i < num_to_change:
                 snack_name: str = input('Input snack name') + '\n'
                 snack_availability: str = input('Type True for yes and False for no') + '\n'
