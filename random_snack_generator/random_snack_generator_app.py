458f422fdb2dfbd0698c7a0f5f07dadca3f8b0b4
eden <eden.fenster@gmail.com>
2022-08-01 10:21:29 +0300

    Made it a bit more efficient.

--- random_snack_generator/random_snack_generator_app.py
+++ random_snack_generator/random_snack_generator_app.py
@@ -35,6 +35,7 @@ def change_the_availability():
     num: int = int(input('How many snacks do you want to change ? \n'))
     while num < 1:
         num: int = int(input('Not a valid number, try again ! \n'))
+
     num_to_change: int = int(num)
     if num_to_change == 1:
         snack_name: str = input('Input snack name \n')
@@ -44,18 +45,18 @@ def change_the_availability():
         food_list.change_availability(snack_name, snack_availability)
         # Prints the current status of the food after we changed it.
         print(food_list.food_lookup_by_name(snack_name))
-    if num_to_change > 1:
-        i: int = 0
+        return
 
-        while i < num_to_change:
-            snack_name: str = input('Input snack name \n')
-            # Prints the current status of the food for reference.
-            print(food_list.food_lookup_by_name(snack_name))
-            snack_availability: str = input('Type True for yes and False for no \n')
-            food_list.change_availability(snack_name, snack_availability)
-            # Prints the current status of the food after we changed it.
-            print(food_list.food_lookup_by_name(snack_name))
-            i += 1
+    i: int = 0
+    while i < num_to_change:
+        snack_name: str = input('Input snack name \n')
+        # Prints the current status of the food for reference.
+        print(food_list.food_lookup_by_name(snack_name))
+        snack_availability: str = input('Type True for yes and False for no \n')
+        food_list.change_availability(snack_name, snack_availability)
+        # Prints the current status of the food after we changed it.
+        print(food_list.food_lookup_by_name(snack_name))
+        i += 1
 
 
 def print_a_snack():
@@ -63,6 +64,7 @@ def print_a_snack():
         input('Do you want a random snack or all snacks printed ? type Random for random and All for all \n')
     if random_or_all == 'Random':
         print(food_list.choose_random())
+        return
     if random_or_all == 'All':
         print(food_list.show_all())
 
@@ -74,6 +76,7 @@ def lookup_a_snack():
     if name_or_availability == 'Name':
         name: str = input('Type name of snack \n')
         print(food_list.food_lookup_by_name(name))
+        return
     if name_or_availability == 'Availability':
         condition: str = input('Type True for available and False for not available \n')
         print(food_list.food_lookup_by_availability(condition))
@@ -86,6 +89,7 @@ def delete_a_snack():
         food_list.delete_one(food_to_delete)
         # Prints the current status of the database after we deleted the food.
         print(food_list.show_all())
+        return
     if one_or_all == 'All':
         food_list.delete_many(food_to_delete)
         # Prints the current status of the database after we deleted the food.
@@ -95,7 +99,8 @@ def delete_a_snack():
 def add_a_snack():
     num: int = int(input('How many snacks do you want to add ? \n'))
     while num < 1:
-        num: int = int(input('Not a vaild number, try again ! \n'))
+        num: int = int(input('Not a valid number, try again ! \n'))
+
     num_to_add: int = int(num)
     if num_to_add == 1:
         snack_name: str = input('Input snack name \n')
@@ -103,15 +108,16 @@ def add_a_snack():
         food_list.add_one(snack_name, snack_availability)
         # Prints the current status of the food after we added the snack.
         print(food_list.food_lookup_by_name(snack_name))
-    if num_to_add > 1:
-        snacks_to_add: List[Tuple[str, str]] = []
+        return
 
-        for i in range(0, num_to_add):
-            snack_name: str = input('Input snack name \n')
-            snack_availability: str = input('Type True for yes and False for no \n')
-            snack: Tuple[str, str] = (snack_name, snack_availability)
-            snacks_to_add.append(snack)
-            food_list.add_many(snacks_to_add)
+    snacks_to_add: List[Tuple[str, str]] = []
+
+    for i in range(0, num_to_add):
+        snack_name: str = input('Input snack name \n')
+        snack_availability: str = input('Type True for yes and False for no \n')
+        snack: Tuple[str, str] = (snack_name, snack_availability)
+        snacks_to_add.append(snack)
+        food_list.add_many(snacks_to_add)
 
 
 if __name__ == '__main__':
