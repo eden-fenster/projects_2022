f6e793519d3fb0f473b6a28c2abcd2731c420dce
eden-fenster <eden.fenster@gmail.com>
2022-10-03 21:29:28 +0300

    The sub databases are children.

---
+++ music_finder/music_database.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import pyodbc
+
+
+class MusicDatabase:
+    def __init__(self, database_path: str , table_name: str):
+        self._connection = pyodbc.connect("Driver=SQLite3;Database="f"{database_path}")
+        self._cursor = self._connection.cursor()
+        self.table_name = table_name
+        pass
+
+    def show_all(self):
+        self._cursor.execute(f'select artist from {self.table_name}')
+        items = self._cursor.fetchall()
+        for item in items:
+            print(item)
+
+    def add_one_artist(self, artist: str):
+        self._cursor.execute(f'insert into {self.table_name} (artist_name) values (?)', artist)
+        self._connection.commit()
+
+    def add_many_artists(self, artist_list: List[str]):
+        self._cursor.executemany(f'insert into {self.table_name} values (?)', artist_list)
+        self._connection.commit()
+
+    def delete_artist(self, artist: str):
+        self._cursor.execute(f'delete from {self.table_name} where artist_name = ?', artist)
+        self._connection.commit()
+
+    def artist_lookup_by_name(self, artist: str):
+        self._cursor.execute(f'select artist_name from {self.table_name} where artist_name = ?', artist)
+        for row in self._cursor:
+            print(row.artist_name)
+
+    def choose_random_artist(self):
+        self._cursor.execute(f'select artist_name from {self.table_name} order by random()')
+        item = self._cursor.fetchone()
+        print(item.artist_name)
--- music_finder/rock_music_database.py
+++ music_finder/rock_music_database.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python3
-import sqlite3
-from typing import List
+from music_finder.music_database import MusicDatabase
 
-import pyodbc
-
-
-class RockMusicDatabase:
-
-    def __init__(self, database_path: str = 'rock.db'):
-        self._connection = pyodbc.connect("Driver=SQLite3;Database="f"{database_path}")
-        self._cursor = self._connection.cursor()
-        pass
-
-    def show_all(self):
-        self._cursor.execute("select artist from rock")
-        items = self._cursor.fetchall()
-        for item in items:
-            print(item)
-
-    def add_one_artist(self, artist: str):
-        self._cursor.execute("insert into rock (artist_name) values (?)", artist)
-        self._connection.commit()
-
-    def add_many_artists(self, list: List[str]):
-        self._cursor.executemany("insert into rock values (?)", list)
-        self._connection.commit()
-
-    def delete_artist(self, artist: str):
-        self._cursor.execute("delete from rock where artist_name = ?", artist)
-        self._connection.commit()
-
-    def artist_lookup_by_name(self, artist: str):
-        self._cursor.execute("select artist_name from rock where artist_name = ?", artist)
-        for row in self._cursor:
-            print(row.artist_name)
-
-    def choose_random_artist(self):
-        self._cursor.execute("select artist_name from rock order by random()")
-        item = self._cursor.fetchone()
-        print(item.artist_name)
+DEFAULT_DATABASE_NAME = 'rock.db'
+DEFAULT_TABLE_NAME = "rock"
 
 
+class RockMusicDatabase(MusicDatabase):
+    def __init__(self):
+        super().__init__(database_path=DEFAULT_DATABASE_NAME, table_name=DEFAULT_TABLE_NAME)
+
+    pass
