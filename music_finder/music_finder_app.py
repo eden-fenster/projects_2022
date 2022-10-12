94a505e94ce81766530a0ee5f5d7b95b6794982e
eden-fenster <eden.fenster@gmail.com>
2022-10-12 06:19:04 +0300

    Adding things for artist

--- music_finder/music_finder_app.py
+++ music_finder/music_finder_app.py
@@ -6,7 +6,8 @@ from music_database import MusicDatabase
 from database_template import create_database
 
 genre_list = MusicFinderDatabase()
-# single_genre_database = MusicDatabase()
+rock_database = MusicDatabase()
+pop_database = MusicDatabase()
 
 
 def main() -> None:
@@ -24,14 +25,23 @@ def main() -> None:
     if add_genre == 'Yes':
         add_a_genre()
 
+    # if add_artist == 'Yes':
+    #     add_an_artist()
+
     if delete_genre == 'Yes':
         delete_a_genre()
 
     if lookup_genre == 'Yes':
         lookup_a_genre()
 
+    # if lookup_artist == 'Yes':
+    #     lookup_an_artist()
+
     if print_genre_or_not == 'Yes':
         print_genre()
+
+    # if print_artist_or_not == 'Yes':
+    #     print_artist()
 
 
 def print_genre():
@@ -47,6 +57,13 @@ def print_genre():
 def lookup_a_genre():
     name: str = input('Type name of genre \n')
     print(genre_list.genre_lookup(name))
+
+# def lookup_an_artist():
+#     name: str = input('Type name of artist \n')
+#     for genre in genre_list:
+#         if name in f'{genre[1]}':
+#
+#     print(genre_list.genre_lookup(name))
 
 
 def delete_a_genre():
@@ -70,6 +87,9 @@ def add_a_genre():
         genre_url: str = input('Input URL of database \n')
 
         genre_list.add_genre(genre_name, genre_url)
+        genre_n: str = genre_name
+        database_name = f'{genre_n}' + '_music_database'
+        database_name = MusicDatabase(database_path=f'{genre_n}.db', table_name=f'{genre_n}')
         # Prints the current status of the food after we added the snack.
         print(genre_list.genre_lookup(genre_name))
         return
@@ -82,6 +102,35 @@ def add_a_genre():
         genre: Tuple[str, str] = (genre_name, genre_url)
         genres_to_add.append(genre)
         genre_list.add_many_genres(genres_to_add)
+
+
+# def add_an_artist():
+#     num: int = int(input('How many artists do you want to add ? \n'))
+#     while num < 1:
+#         num: int = int(input('Not a valid number, try again ! \n'))
+#
+#     num_to_add: int = int(num)
+#     if num_to_add == 1:
+#         exist: bool = bool(input('Does the database exist ? \n'))
+#         if not exist:
+#             genre_name: str = input('Input genre name \n')
+#             create_database(name=genre_name)
+#         genre_name: str = input('Input genre name \n')
+#         genre_url: str = input('Input URL of database \n')
+#
+#         genre_list.add_genre(genre_name, genre_url)
+#         # Prints the current status of the food after we added the snack.
+#         print(genre_list.genre_lookup(genre_name))
+#         return
+#
+#     genres_to_add: List[Tuple[str, str]] = []
+#
+#     for i in range(0, num_to_add):
+#         genre_name: str = input('Input genre name \n')
+#         genre_url: str = input('Input URL of database \n')
+#         genre: Tuple[str, str] = (genre_name, genre_url)
+#         genres_to_add.append(genre)
+#         genre_list.add_many_genres(genres_to_add)
 
 
 if __name__ == '__main__':
