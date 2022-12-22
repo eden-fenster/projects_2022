c3547e505fffeb2fc928a765b8b66afd48e6d568
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2022-12-22 07:52:14 +0200

    trying to debug

--- practice/subsets.py
+++ practice/subsets.py
@@ -3,25 +3,26 @@ from typing import List
 
 
 def subset_wrapper(a: List[int], k: int) -> List[int]:
-    new_array: List[int] = [] * len(a)
+    new_array: List[int] = []
     return subset(a=a, i=0, j=0, k=k, new_array=new_array)
 
 
 def subset(a: List[int], i: int, j: int, k: int, new_array: List[int]) -> List[int]:
-    if i < len(a) - 1 and k > 0:
-        without_subtraction: List[int] = subset(a=a, i=i + 1, j=j, k=k, new_array=new_array)
-        value = a[i]
-        new_array[j] = value
-        with_subtraction: List[int] = subset(a=a, i=i + 1, j=j + 1, k=k - value, new_array=new_array)
-        return without_subtraction or with_subtraction
+    if i >= len(a) - 1 or k == 0:
+        return new_array
     if k < 0 and i < len(a) - 1:
         value = new_array.pop()
         return subset(a=a, i=i + 1, j=j - 1, k=k + value, new_array=new_array)
-    if k != 0 and i == len(a) - 1:
-        return []
     if k == 0 and i == 0:
         return [0]
-    return new_array
+    print(f'currently on {a[i]}')
+    without_subtraction: List[int] = subset(a=a, i=i + 1, j=j, k=k, new_array=new_array)
+    value = a[i]
+    print(f'{value} is being added to new_array')
+    with_subtraction: List[int] = subset(a=a, i=i + 1, j=j + 1, k=k - value, new_array=new_array)
+    new_array.append(value)
+    return without_subtraction or with_subtraction
+
 
 
 def test_subset():
