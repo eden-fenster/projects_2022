dd738f9ed6e8652b0254ca3b9497863a406f43b8
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-01-22 18:45:55 +0200

    trying to debug

--- practice/calc.py
+++ practice/calc.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 def calc_wrapper(num: int, result: int, max_op: int) -> int:
     return calc(num=num, result=result, max_op=max_op,
-                num_of_calculations=0, result_up_to_this_point=0, history_of_calculations='', right_solutions=0)
+                num_of_calculations=0, result_up_to_this_point=0, history_of_calculations='')
 
 
 def calc(num: int, result: int, max_op: int, num_of_calculations: int,
-         result_up_to_this_point: int, history_of_calculations: str, right_solutions: int) -> int:
+         result_up_to_this_point: int, history_of_calculations: str) -> int:
     # If success, return history + add to counter of right solutions.
     if result_up_to_this_point == result:
-        print(string_history(num=num, history_of_calculations=history_of_calculations,
-                             num_of_calculations=num_of_calculations, num_in_history=0))
-        return right_solutions + 1
+        # print(string_history(num=num, history_of_calculations=history_of_calculations,
+        # num_of_calculations=num_of_calculations, num_in_history=0))
+        return 1
     # If failure, return 0.
     if num_of_calculations == max_op:
         return 0
     # Else, go over the 4 operations.
     add: int = calc(num=num, result=result, max_op=max_op, num_of_calculations=num_of_calculations + 1,
                     result_up_to_this_point=result_up_to_this_point + num,
-                    history_of_calculations=history_of_calculations + "+", right_solutions=right_solutions)
+                    history_of_calculations=history_of_calculations + "+")
     subtract: int = calc(num=num, result=result, max_op=max_op, num_of_calculations=num_of_calculations + 1,
                          result_up_to_this_point=result_up_to_this_point - num,
-                         history_of_calculations=history_of_calculations + "-", right_solutions=right_solutions)
+                         history_of_calculations=history_of_calculations + "-")
     multiply: int = calc(num=num, result=result, max_op=max_op, num_of_calculations=num_of_calculations + 1,
                          result_up_to_this_point=result_up_to_this_point * num,
-                         history_of_calculations=history_of_calculations + "*", right_solutions=right_solutions)
+                         history_of_calculations=history_of_calculations + "*")
     divide: int = calc(num=num, result=result, max_op=max_op, num_of_calculations=num_of_calculations + 1,
                        result_up_to_this_point=result_up_to_this_point // num,
-                       history_of_calculations=history_of_calculations + "/", right_solutions=right_solutions)
+                       history_of_calculations=history_of_calculations + "/")
     # Return the sum of # of legal right solutions.
     return add + subtract + multiply + divide
 
