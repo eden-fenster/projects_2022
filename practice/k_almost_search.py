09508fe429cee1c73c40a61e28960b4cb2cd6cc5
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-01-24 16:24:41 +0200

    k_almost_Search

---
+++ practice/k_almost_search.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def k_almost_search(almost_sorted_list: List[int], num: int) -> int:
+    # Looping over the list.
+    for number in almost_sorted_list:
+        if almost_sorted_list[number] < num:
+            continue
+        # If failure, return -1
+        if almost_sorted_list[number] > num:
+            return -1
+        # If success, return index of number in List.
+        return number
+
+
+def test_k_almost_search():
+    almost_sorted_list: List[int] = [3, 0, 0, 4, 7, 9, 0, 0, 0, 0, 11, 15, 0, 19, 20, 0, 0, 31, 40, 0]
+    assert k_almost_search(almost_sorted_list=almost_sorted_list, num=9) == 5
+    assert k_almost_search(almost_sorted_list=almost_sorted_list, num=31) == 17
+    assert k_almost_search(almost_sorted_list=almost_sorted_list, num=30) == -1
