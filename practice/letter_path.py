dc6f4c11be9a6b814b7be1ff289e297f97a12792
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-01-25 16:20:49 +0200

    Currently trying to debug max_path

--- practice/letter_path.py
+++ practice/letter_path.py
@@ -8,36 +8,47 @@ def length_path_wrapper(mat: List[List[str]], pattern: str) -> int:
 
 def length_path(mat: List[List[str]], row: int, col: int, pattern: str, length_of_path: int) -> int:
     # If failure, return length of path.
-    if mat[row][col] not in pattern \
-            or row < 0 or col < 0 or row > len(mat) - 1 or col > len(mat[0]) - 1 or mat[row][col] == -1:
+    if row < 0 or col < 0 or \
+            row > len(mat) - 1 or col > len(mat[0]) - 1 or mat[row][col] == -1:
+        return length_of_path
+    if mat[row][col] not in pattern:
         return length_of_path
 
     # Mark current position and move down search path.
     mat[row][col] = str(-1)
-    north: int = length_path(mat=mat, row=row - 1, col=col, pattern=pattern, length_of_path=length_of_path + 1)
-    south: int = length_path(mat=mat, row=row + 1, col=col, pattern=pattern, length_of_path=length_of_path + 1)
-    east: int = length_path(mat=mat, row=row, col=col + 1, pattern=pattern, length_of_path=length_of_path + 1)
-    west: int = length_path(mat=mat, row=row, col=col - 1, pattern=pattern, length_of_path=length_of_path + 1)
+    north: int = length_path(mat=mat, row=row - 1, col=col,
+                             pattern=pattern, length_of_path=length_of_path + 1)
+    south: int = length_path(mat=mat, row=row + 1, col=col,
+                             pattern=pattern, length_of_path=length_of_path + 1)
+    east: int = length_path(mat=mat, row=row, col=col + 1,
+                            pattern=pattern, length_of_path=length_of_path + 1)
+    west: int = length_path(mat=mat, row=row, col=col - 1,
+                            pattern=pattern, length_of_path=length_of_path + 1)
 
     # Finding and returning the biggest result, if result bigger than 0, success.
     return max(north, south, east, west)
 
 
 def max_path_wrapper(mat: List[List[str]], pattern: str) -> int:
-    return max_path(mat=mat, row=0, col=0, pattern=pattern)
+    # Creating a list where we mark after visiting a position.
+    rows, cols = (len(mat), len(mat[0]))
+    marked: List[List[bool]] = [[False] * cols] * rows
+    return max_path(mat=mat, row=0, col=0, pattern=pattern, marked=marked)
 
 
-def max_path(mat: List[List[str]], row: int, col: int, pattern: str) -> int:
+def max_path(mat: List[List[str]], row: int, col: int, pattern: str, marked: List[List[bool]]) -> int:
     # If illegal coordinates, failure and return 0.
-    if row < 0 or col < 0 or row > len(mat) - 1 or col > len(mat[0]) - 1:
+    if row < 0 or col < 0 or row > len(mat) - 1 or col > len(mat[0]) - 1 or marked[row][col]:
         return 0
     # Calculating path starting from[row][col]
     path: int = length_path(mat=mat, row=row, col=col, pattern=pattern, length_of_path=0)
-    # Moving down the search path and calculating path with a starting point at every position.
-    north: int = max_path(mat=mat, row=row - 1, col=col, pattern=pattern)
-    south: int = max_path(mat=mat, row=row + 1, col=col, pattern=pattern)
-    east: int = max_path(mat=mat, row=row, col=col + 1, pattern=pattern)
-    west: int = max_path(mat=mat, row=row, col=col - 1, pattern=pattern)
+    # Marking our position and moving down the search path and calculating path with a starting point at every position.
+    marked[row][col] = True
+    north: int = max_path(mat=mat, row=row - 1, col=col, pattern=pattern, marked=marked)
+    south: int = max_path(mat=mat, row=row + 1, col=col, pattern=pattern, marked=marked)
+    east: int = max_path(mat=mat, row=row, col=col + 1, pattern=pattern, marked=marked)
+    west: int = max_path(mat=mat, row=row, col=col - 1, pattern=pattern, marked=marked)
+
     # If max > 0, success.
     return max(path, north, south, east, west)
 
