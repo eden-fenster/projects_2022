d512543936fceb378ae606215075624639636f68
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-01-29 19:39:23 +0200

    Updating

--- practice/missing_value.py
+++ practice/missing_value.py
@@ -2,13 +2,20 @@
 from typing import List
 
 
-def missing_value(sorted_list: int) -> int:
+def missing_value(sorted_list: List[int]) -> int:
     diff: int = 0
     # Looping over the list.
-    for location in range(0, sorted_list - 2):
+    for location in range(0, len(sorted_list) - 2):
         # If diff between the soon is the same, continue.
         if sorted_list[location + 1] - sorted_list[location] == sorted_list[location + 2] - sorted_list[location + 1]:
             diff = sorted_list[location + 1] - sorted_list[location]
             continue
         # Else, return number -> smaller number in diff equation + regular diff.
+        if location == 0:
+            return sorted_list[location] + diff
         return sorted_list[location + 1] + diff
+
+
+def test_missing_value():
+    sorted_list: List[int] = [7, 10, 13, 16, 22, 25]
+    assert missing_value(sorted_list=sorted_list) == 19
