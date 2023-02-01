df2c2f8a904b44462b946f53cfc331b40718d7db
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-02-01 06:35:12 +0200

    Fixed it

--- practice/pattern.py
+++ practice/pattern.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
 
 def same_pattern_wrapper(first_string: str, second_string: str) -> bool:
-    return same_pattern(first_string=first_string, second_string=second_string,
-                        location_in_second=0, pattern_from_second='')
+    return same_pattern(first_string=first_string, pattern_string=second_string,
+                        location_in_first=0, location_in_pattern=0, pattern_from_string='')
 
 
-def same_pattern(first_string: str, second_string: str, location_in_second: int, pattern_from_second: str) -> bool:
-    # print(f'Pattern -> {pattern_from_second}')
+def same_pattern(first_string: str, pattern_string: str,
+                 location_in_first: int, location_in_pattern: int, pattern_from_string: str) -> bool:
+    print(f'Pattern -> {pattern_from_string}')
     # If second string is just an *, return true.
-    if second_string is '*':
+    if pattern_string is '*':
         return True
     # If both are the same, return true.
-    if first_string is second_string:
+    if first_string is pattern_string:
         return True
     # If pattern from second not in first, return False.
-    if pattern_from_second not in first_string:
+    if pattern_from_string not in first_string:
         return False
     # If we reached the end of the second string, return True.
-    if location_in_second >= len(second_string):
+    if location_in_pattern >= len(pattern_string):
         return True
+    # If we reached the end of the first string, return true.
+    if location_in_first >= len(first_string):
+        return False
     # If we reached an *, the pattern that we are looking for is reset.
-    if second_string[location_in_second] is '*':
-        return same_pattern(first_string=first_string, second_string=second_string,
-                            location_in_second=location_in_second + 1, pattern_from_second='')
+    if pattern_string[location_in_pattern] is '*':
+        return same_pattern(first_string=first_string, pattern_string=pattern_string,
+                            location_in_first=location_in_first,
+                            location_in_pattern=location_in_pattern + 1, pattern_from_string='') \
+               or same_pattern(first_string=first_string, pattern_string=pattern_string,
+                               location_in_first=location_in_first + 1, location_in_pattern=location_in_pattern,
+                               pattern_from_string=pattern_from_string)
     # Else, continue.
-    return same_pattern(first_string=first_string, second_string=second_string,
-                        location_in_second=location_in_second + 1,
-                        pattern_from_second=pattern_from_second + second_string[location_in_second])
+    return same_pattern(first_string=first_string, pattern_string=pattern_string,
+                        location_in_first=location_in_first + 1,
+                        location_in_pattern=location_in_pattern + 1,
+                        pattern_from_string=pattern_from_string + pattern_string[location_in_pattern])
 
 
 def test_same_pattern():
