307d60fd2fbe9ef2607b5572bb89cf9aa610cf5c
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-02-01 16:03:43 +0200

    Fixed it

--- practice/match.py
+++ practice/match.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 from typing import List
 
+two_digits_only: int = 2
+one_digit_only: int = 1
+either_one_or_two: int = 0
+
 
 def match_wrapper(numbers: List[int], pattern: List[int]) -> bool:
     return match(numbers=numbers, pattern=pattern, location_in_numbers=0, location_in_pattern=0)
@@ -19,11 +23,14 @@ def match(numbers: List[int], pattern: List[int], location_in_numbers: int, loca
     # If we have not found anything, false.
     if location_in_numbers == len(numbers):
         return False
-    # If it's a single digit.
-    if numbers[location_in_numbers] < 10:
-        # If not part of pattern, reset
-        if pattern[location_in_pattern] == 2:
-            # If end of list is reached, false.
+    # If triple digit, move one number and reset pattern.
+    if numbers[location_in_numbers] >= 100:
+        return match(numbers=numbers, pattern=pattern,
+                     location_in_numbers=location_in_numbers + 1, location_in_pattern=0)
+    # If pattern is two digit only.
+    if pattern[location_in_pattern] == two_digits_only:
+        # If it's a single digit.
+        if numbers[location_in_numbers] < 10:
             if location_in_numbers == len(numbers) - 1:
                 return False
             return match(numbers=numbers, pattern=pattern,
@@ -33,21 +40,24 @@ def match(numbers: List[int], pattern: List[int], location_in_numbers: int, loca
             return True
         return match(numbers=numbers, pattern=pattern, location_in_numbers=location_in_numbers + 1,
                      location_in_pattern=location_in_pattern + 1)
-    # If it's a double digit.
-    if 10 <= numbers[location_in_numbers] < 100:
-        # If not part of pattern, reset
-        if pattern[location_in_pattern] == 1:
+    # If pattern is one digit only.
+    if pattern[location_in_pattern] == one_digit_only:
+        # If it's a double digit.
+        if 10 <= numbers[location_in_numbers] < 100:
             if location_in_numbers == len(numbers) - 1:
                 return False
             return match(numbers=numbers, pattern=pattern,
                          location_in_numbers=location_in_numbers, location_in_pattern=0)
+        # If end of pattern is reached, return true.
         if location_in_pattern == len(pattern) - 1:
             return True
         return match(numbers=numbers, pattern=pattern, location_in_numbers=location_in_numbers + 1,
                      location_in_pattern=location_in_pattern + 1)
-    # If triple digit, move one number and reset pattern.
-    return match(numbers=numbers, pattern=pattern,
-                 location_in_numbers=location_in_numbers + 1, location_in_pattern=0)
+    # If it's either type of digit.
+    if location_in_pattern == len(pattern) - 1:
+        return True
+    return match(numbers=numbers, pattern=pattern, location_in_numbers=location_in_numbers + 1,
+                 location_in_pattern=location_in_pattern + 1)
 
 
 def test_match():
