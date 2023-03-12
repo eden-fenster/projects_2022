250ff78b94663f5918e20532f94f0122d3faf916
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-03-12 14:24:05 +0200

    Trying to make some internet code work.

---
+++ practice/sudoku/soduku.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# Gets from user a 9 x 9 board.
+import logging
+from typing import List
+import argparse
+from soduku_class import Sudoku
+
+
+# Check if all numbers are between 1 - n.
+# If no, fail.
+# If yes, check for each row, col and k * l sub-table.
+# Check that each number doesn't appear more than once at the 3 things above.
+def get_args():
+    # Getting a file that contains a partially filled sudoku from the command line.
+    parser = argparse.ArgumentParser \
+        (description='Sudoku', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file', metavar='file', help='Input file')
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+    # sub_square_size: int = int(input("What is the size of the sub_square ? "))
+    file_to_open = args.file
+    print(create_sudoku(read_file(file_to_open=file_to_open)))
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Reads in a file
+def read_file(file_to_open) -> List[str]:
+    list_of_lines: List[str] = []
+    # Reading the file by line.
+    with open(file_to_open, 'r', encoding='UTF-8') as file:
+        for line in file.readlines():
+            list_of_lines.append(line.rstrip())
+    return list_of_lines
+
+
+# Converts a file into a two-dimensional list.
+def create_sudoku(list_of_lines: List[str]) -> List[List[int]]:
+    # Converting the lines into numbers, creating a two-dimensional list of ints.
+    sudoku: List[List[int]] = []
+    sudoku_line: List[int] = []
+    for line in list_of_lines:
+        for number in line:
+            if not number.isdigit():
+                logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+                logging.warning('Not a number')
+                continue
+            sudoku_line.append(int(number))
+        sudoku.append(sudoku_line)
+        sudoku_line = []
+    return sudoku
+
+
+# Fills the sudoku.
+# def fill_sudoku(sudoku: List[List[int]], current_row: int, current_column: int,
+#                 smallest_number: int, largest_number: int, sub_square_size: int) -> List[List[int]]:
+#     # If we went over all the rows, return filled sudoku table.
+#     if current_row >= len(sudoku):
+#         return sudoku
+#
+#     # If we finished a row, go to the next row.
+#     if current_column >= len(sudoku[current_row]):
+#         return fill_sudoku(sudoku=sudoku, current_row=current_row + 1, current_column=0,
+#                            smallest_number=smallest_number, largest_number=largest_number,
+#                            sub_square_size=sub_square_size)
+#
+#     # Adding in numbers to row.
+#     for number in range(smallest_number, largest_number + 1):
+#         can_i_put_num: bool = True
+#         # Is # already in row ?
+#         if number in sudoku[current_row]:
+#             continue
+#         # Is # already in column ?
+#         for actual_row in sudoku:
+#             # If # not in col, continue, else, break.
+#             if actual_row[current_column] != number:
+#                 continue
+#             can_i_put_num = False
+#             break
+#         # TODO : Backtracking if something goes wrong.
+#         # Is # inside sub - table ?
+#         start_row: int = current_row - current_row % sub_square_size
+#         start_col: int = current_column - current_column % sub_square_size
+#         for row in range(sub_square_size):
+#             for column in range(sub_square_size):
+#                 if sudoku[row + start_row][column + start_col] != number:
+#                     continue
+#                 can_i_put_num = False
+#                 break
+#         # Is it already in square ?
+#         # Putting it in.
+#         if can_i_put_num and sudoku[current_row][current_column] == 0:
+#             sudoku[current_row][current_column] = number
+#     print(f'Number -> {sudoku[current_row][current_column]}')
+#     # Moving down the search path to the next col in the row.
+#     return fill_sudoku(sudoku=sudoku, current_row=current_row, current_column=current_column + 1,
+#                        smallest_number=smallest_number,
+#                        largest_number=largest_number, sub_square_size=sub_square_size)
+
+
+def test_fill_sudoku():
+    sudoku: List[List[int]] = \
+        create_sudoku(read_file(file_to_open="../test.txt"))
+    first: Sudoku = Sudoku(sudoku, 4)
+    assert first.solve_sudoku(2) == [[3, 1, 2, 4], [2, 4, 1, 3], [1, 3, 4, 2], [4, 2, 3, 1]]
+    # assert fill_sudoku(sudoku=sudoku, current_row=0, current_column=0,
+    #                   smallest_number=1, largest_number=4, sub_square_size=2) \
+    #       == [[3, 1, 2, 4], [2, 4, 1, 3], [1, 3, 4, 2], [4, 2, 3, 1]]
+    second_sudoku: List[List[int]] = \
+        create_sudoku(read_file(file_to_open="../test3.txt"))
+    second: Sudoku = Sudoku(second_sudoku, 4)
+    assert second.solve_sudoku(2) == [[2, 4, 3, 1], [3, 1, 2, 4], [1, 3, 4, 2], [4, 2, 1, 3]]
+    # assert fill_sudoku(sudoku=second_sudoku, current_row=0, current_column=0, smallest_number=1, largest_number=4,
+    #                   sub_square_size=2) \
+    #       == [[2, 4, 3, 1], [3, 1, 2, 4], [1, 3, 4, 2], [4, 2, 1, 3]]
+    # second_sudoku: List[List[int]] = \
+    #     create_sudoku(read_file(file_to_open="test2.txt"))
+    # assert fill_sudoku(sudoku=second_sudoku,
+    #                    row=0, col=0, smallest_number=1, largest_number=9) \
+    #        == [[8, 1, 2, 3, 6, 5, 7, 4, 9],
+    #            [5, 7, 3, 2, 9, 4, 6, 1, 8],
+    #            [6, 4, 9, 7, 1, 8, 5, 2, 3],
+    #            [7, 8, 6, 4, 3, 2, 9, 5, 1],
+    #            [9, 5, 1, 6, 8, 7, 2, 3, 4],
+    #            [2, 3, 4, 9, 5, 1, 8, 6, 7],
+    #            [1, 6, 8, 5, 7, 3, 4, 9, 2],
+    #            [4, 9, 7, 1, 2, 6, 3, 8, 5],
+    #            [3, 2, 5, 8, 4, 9, 1, 7, 6]]
---
+++ practice/sudoku/soduku_class.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+from copy import deepcopy
+from typing import List, Tuple, Set
+
+SIZE = 4
+BOX_SIZE = 2
+
+
+class Sudoku:
+    def __init__(self, grid: List[List[int]]):
+        n = len(grid)
+        self.grid = grid
+        self.n = n
+        # create a grid of viable candidates for each position
+        candidates = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                if grid[i][j] == 0:
+                    row.append(self.find_options(i, j))
+                else:
+                    row.append(set())
+            candidates.append(row)
+        self.candidates = candidates
+
+    def __repr__(self) -> str:
+        repr = ''
+        for row in self.grid:
+            repr += str(row) + '\n'
+        return repr
+
+    def get_row(self, r: int) -> List[int]:
+        return self.grid[r]
+
+    def get_col(self, c: int) -> List[int]:
+        return [row[c] for row in self.grid]
+
+    def get_box_inds(self, r: int, c: int) -> List[Tuple[int, int]]:
+        inds_box = []
+        i0 = (r // BOX_SIZE) * BOX_SIZE  # get first row index
+        j0 = (c // BOX_SIZE) * BOX_SIZE  # get first column index
+        for i in range(i0, i0 + BOX_SIZE):
+            for j in range(j0, j0 + BOX_SIZE):
+                inds_box.append((i, j))
+        return inds_box
+
+    def get_box(self, r: int, c: int) -> List[int]:
+        box = []
+        for i, j in self.get_box_inds(r, c):
+            box.append(self.grid[i][j])
+        return box
+
+    def find_options(self, r: int, c: int) -> Set:
+        nums = set(range(1, SIZE + 1))
+        set_row = set(self.get_row(r))
+        set_col = set(self.get_col(c))
+        set_box = set(self.get_box(r, c))
+        used = set_row | set_col | set_box
+        valid = nums.difference(used)
+        return valid
+
+    def place_and_erase(self, r: int, c: int, x: int, constraint_prop=True):
+        """ remove x as a candidate in the grid in this row, column and box"""
+        # place candidate x
+        self.grid[r][c] = x
+        self.candidates[r][c] = set()
+        # remove candidate x in neighbours
+        inds_row = [(r, j) for j in range(self.n)]
+        inds_col = [(i, c) for i in range(self.n)]
+        inds_box = self.get_box_inds(r, c)
+        erased = [(r, c)]  # set of indices for constraint propagation
+        erased += self.erase([x], inds_row + inds_col + inds_box, [])
+        # constraint propagation, through every index that was changed
+        while erased and constraint_prop:
+            i, j = erased.pop()
+            inds_row = [(i, j) for j in range(self.n)]
+            inds_col = [(i, j) for i in range(self.n)]
+            inds_box = self.get_box_inds(i, j)
+            for inds in [inds_row, inds_col, inds_box]:
+                # apply strategies
+                # 1. hidden singles
+                uniques = self.get_unique(inds)
+                for inds_unique, num in uniques:
+                    i_u, j_u = inds_unique[0]
+                    self.candidates[i_u][j_u] = set(num)
+                    erased += self.erase(num, inds, inds_unique)
+
+    def erase(self, nums, indices, keep):
+        """ erase nums as candidates in indices, but not in keep"""
+        erased = []
+        for i, j in indices:
+            edited = False
+            if (i, j) in keep:
+                continue
+            for x in nums:
+                if x in self.candidates[i][j]:
+                    self.candidates[i][j].remove(x)
+                    edited = True
+            if edited:
+                erased.append((i, j))
+        return erased
+
+    def count_candidates(self, indices):
+        count = [[] for _ in range(self.n + 1)]
+        for i, j in indices:
+            for num in self.candidates[i][j]:
+                count[num].append((i, j))
+        return count
+
+    def get_unique(self, indices):
+        groups = self.count_candidates(indices)
+        uniques = []  # final set of unique candidates to return
+        for num, group_inds in enumerate(groups):
+            if len(group_inds) == 1:
+                uniques.append((group_inds, [num]))
+        return uniques
+
+    def solve_sudoku(grid, num_boxes=SIZE, all_solutions=False):
+        def solve(puzzle, depth=0):
+            nonlocal calls, depth_max
+            calls += 1
+            depth_max = max(depth, depth_max)
+            solved = False
+            while not solved:
+                solved = True
+                edited = False  # if no edits, either done or stuck
+                for i in range(n):
+                    for j in range(n):
+                        if puzzle.grid[i][j] == 0:
+                            solved = False
+                            options = puzzle.candidates[i][j]
+                            if len(options) == 0:
+                                return False  # this call is going nowhere
+                            elif len(options) == 1:  # Step 1
+                                puzzle.place_and_erase(i, j, list(options)[0])  # Step 2
+                                edited = True
+                if not edited:  # changed nothing in this round -> either done or stuck
+                    if solved:
+                        solution_set.append(grid2str(puzzle.grid))
+                        return True
+                    else:  # Find the square with the least number of options
+                        min_guesses = (n + 1, -1)
+                        for i in range(n):
+                            for j in range(n):
+                                options = puzzle.candidates[i][j]
+                                if len(options) > 1:
+                                    min_guesses = min((len(options), (i, j)), min_guesses)
+                        i, j = min_guesses[1]
+                        options = puzzle.candidates[i][j]
+                        for y in options:  # step 3. backtracking check point:
+                            puzzle_next = deepcopy(puzzle)
+                            puzzle_next.place_and_erase(i, j, y)
+                            solved = solve(puzzle_next, depth=depth + 1)
+                            if solved and not all_solutions:
+                                break  # return 1 solution
+                        return solved
+            return solved
+
+        calls, depth_max = 0, 0
+        solution_set = []
+        puzzle = Sudoku(grid)
+        n = puzzle.n
+
+        solved = solve(puzzle, depth=0)
+        info = {'calls': calls,
+                'max depth': depth_max,
+                'nsolutions': len(solution_set),
+                }
+
+        return solution_set, solved, info
+
+
+def flatten(grid) -> List[int]:
+    arr = []
+    for row in grid:
+        arr.extend(row)
+    return arr
+
+
+def unflatten(arr: List[int], n=9) -> List[List[int]]:
+    grid = []
+    for i in range(0, len(arr), n):
+        grid.append(arr[i:i + n])
+    return grid
+
+
+def arr2str(arr: List[int]) -> str:
+    string = ''
+    for digit in arr:
+        string += str(digit)
+    return string
+
+
+def str2arr(string: str) -> List[int]:
+    arr = []
+    end = string.find('-')
+    end = len(string) if end == -1 else end
+    for c in string[0:end]:
+        if c == '.':
+            arr.append(0)
+        else:
+            arr.append(int(c))
+    return arr
+
+
+def grid2str(grid: List[List[int]]) -> str:
+    return arr2str(flatten(grid))
+
+
+def str2grid(string: str) -> List[List[int]]:
+    return unflatten(str2arr(string))
