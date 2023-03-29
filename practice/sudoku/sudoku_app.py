aab0833f91135a33a90895d79ff3659d847fb06e
eden-fenster <eden.fenster@gmail.com>
2023-03-29 12:06:20 +0300

    Hardcoded attempts with sudoku app in flask

--- practice/sudoku/sudoku_app.py
+++ practice/sudoku/sudoku_app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+import logging
+import sys
+from typing import List
+
+from flask import Flask, render_template
 import soduku
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    pass
+def index():
+    return render_template('index.html')
+
+
+@app.route('/')
+def app():
+    print("Input a file")
+    file = 'test.txt'
+    read_file: List[str] = soduku.read_file(file_to_open=file)
+    if not read_file:
+        logging.error(f"No sudoku found")
+        sys.exit(1)
+    initial_grid: List[List[int]] = soduku.create_sudoku(read_file)
+    soduku.dump_grid(description="Initial grid", grid=initial_grid)
+    solutions, have_solution, information = soduku.solve_sudoku(grid=initial_grid)
+    for i, solution in enumerate(solutions):
+        soduku.dump_grid(description=f"solution {i + 1}", grid=solution)
