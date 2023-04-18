26bec07ee84a6cd78e7a017d1a513df6f3a2cd07
eden-fenster <eden.fenster@gmail.com>
2023-04-18 12:47:20 +0300

    Part 1 of REST tutorial.

---
+++ pets-api/manage.py
@@ -0,0 +1,20 @@
+# Set the path
+import os
+import sys
+from flask_script import *
+from application import create_app
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+app = create_app()
+manager = Manager(app)
+
+# Turn on debugger and reloader.
+manager.add_command("runserver", Server(
+    use_debugger=True,
+    use_reloader=True,
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 5000)))
+)
+
+if __name__ == "__main__":
+    manager.run()
