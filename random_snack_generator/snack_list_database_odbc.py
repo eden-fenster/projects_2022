3e26b9df77928c4cfde59f23fd90433d7209c31d
eden-fenster <eden.fenster@gmail.com>
2023-06-01 09:49:06 +0300

    Fixed a minor bug - changed sqlite3 to sqlite.

--- random_snack_generator/snack_list_database_odbc.py
+++ random_snack_generator/snack_list_database_odbc.py
@@ -7,7 +7,7 @@ import pyodbc
 class FoodDatabase:
 
     def __init__(self, database_path: str = 'food.db'):
-        self._connection = pyodbc.connect("Driver=SQLite3;Database="f"{database_path}")
+        self._connection = pyodbc.connect("Driver=SQLite;Database="f"{database_path}")
         self._cursor = self._connection.cursor()
         pass
 
